9c32579598b2a812305d2cdf509620d0edf719a4
twobetta <sueh9898@gmail.com>
2026-05-24 22:42:34 +0900

    style: 활동 행 지역 라벨의 글꼴 크기를 조정한다

--- app/app/Feature/Activity/View/Component/ActivityListRow.swift
+++ app/app/Feature/Activity/View/Component/ActivityListRow.swift
@@ -43,7 +43,7 @@ struct ActivityListRow: View {
                             
                             Text(region.label)
                         }
-                        .font(.caption2)
+                        .font(.footnote)
                         .foregroundStyle(.secondary)
                     }
                 }
