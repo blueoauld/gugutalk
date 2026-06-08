51f9aca0dec9671e68b7ad7dc56dfd0d64282226
twobetta <sueh9898@gmail.com>
2026-06-08 12:36:56 +0900

    feat: 로그 예외 처리 추가

--- server/src/main/kotlin/com/blueoauld/server/common/filter/RequestLoggingFilter.kt
+++ server/src/main/kotlin/com/blueoauld/server/common/filter/RequestLoggingFilter.kt
@@ -7,13 +7,20 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
+import org.springframework.util.AntPathMatcher
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Component
 class RequestLoggingFilter : OncePerRequestFilter() {
 
+    private val antPathMatcher = AntPathMatcher()
+    private val exclude = listOf(
+        HttpMethod.GET to "/ws/**",
+        HttpMethod.DELETE to "/api/push",
+    )
     private val log = KotlinLogging.logger {}
 
     override fun doFilterInternal(
@@ -32,6 +39,19 @@ class RequestLoggingFilter : OncePerRequestFilter() {
             log.info {
                 "METHOD = ${request.method}, URI = ${request.requestURI}, IP = $ip, STATUS = ${response.status}, MS = $elapsed"
             }
+        }
+    }
+
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        return matches(exclude, request)
+    }
+
+    private fun matches(patterns: List<Pair<HttpMethod, String>>, request: HttpServletRequest): Boolean {
+        val uri = request.requestURI
+        val method = HttpMethod.valueOf(request.method)
+
+        return patterns.any { (m, pattern) ->
+            m == method && antPathMatcher.match(pattern, uri)
         }
     }
 }
